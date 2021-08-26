4bcc03161387949a78b798334e41d50c2601c448
czerwinski <krzysztof.czerwinski@primesoft.pl>
2021-08-26 15:20:21 +0200

    Add basic symfony bundle setup

---
+++ src/DependencyInjection/ConfigSchema.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Kczer\ExcelImporterBundle\DependencyInjection;
+
+class ConfigSchema
+{
+
+}
---
+++ src/ExcelImporterBundle.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Kczer\ExcelImporterBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ExcelImporterBundle extends Bundle
+{
+}
