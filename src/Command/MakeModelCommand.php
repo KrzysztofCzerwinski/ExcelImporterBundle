1c7da9f78bef62340b30f1ce4182cb4d909cf406
czerwinski <krzysztof.czerwinski@primesoft.pl>
2022-03-25 22:10:27 +0100

    Add missing throw tags in make command class

--- src/Command/MakeModelCommand.php
+++ src/Command/MakeModelCommand.php
@@ -4,15 +4,24 @@ declare(strict_types=1);
 namespace Kczer\ExcelImporterBundle\Command;
 
 use Kczer\ExcelImporterBundle\Exception\Annotation\InvalidAnnotationParamException;
+use Kczer\ExcelImporterBundle\Exception\Annotation\InvalidDisplayModelSetterParameterTypeException;
+use Kczer\ExcelImporterBundle\Exception\Annotation\ModelPropertyNotSettableException;
+use Kczer\ExcelImporterBundle\Exception\Annotation\NotExistingModelClassException;
 use Kczer\ExcelImporterBundle\Exception\Annotation\UnexpectedAnnotationOptionException;
+use Kczer\ExcelImporterBundle\Exception\Annotation\UnexpectedColumnExcelCellClassException;
 use Kczer\ExcelImporterBundle\Exception\Annotation\UnexpectedOptionExpectedDataTypeException;
 use Kczer\ExcelImporterBundle\Exception\Annotation\UnexpectedOptionValueDataTypeException;
 use Kczer\ExcelImporterBundle\Exception\DuplicateExcelIdentifierException;
 use Kczer\ExcelImporterBundle\Maker\ModelMaker;
+use PhpOffice\PhpSpreadsheet\Exception;
+use ReflectionException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class MakeModelCommand extends Command
 {
@@ -41,11 +50,20 @@ class MakeModelCommand extends Command
     }
 
     /**
-     * @throws UnexpectedAnnotationOptionException
-     * @throws UnexpectedOptionValueDataTypeException
-     * @throws UnexpectedOptionExpectedDataTypeException
      * @throws DuplicateExcelIdentifierException
      * @throws InvalidAnnotationParamException
+     * @throws UnexpectedAnnotationOptionException
+     * @throws UnexpectedOptionExpectedDataTypeException
+     * @throws UnexpectedOptionValueDataTypeException
+     * @throws InvalidDisplayModelSetterParameterTypeException
+     * @throws ModelPropertyNotSettableException
+     * @throws NotExistingModelClassException
+     * @throws UnexpectedColumnExcelCellClassException
+     * @throws Exception
+     * @throws ReflectionException
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function execute(InputInterface $input, OutputInterface $output): int
     {
